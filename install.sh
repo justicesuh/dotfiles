7664c89d2d665286d99f442fd5bfeb94c5ff1a56
Justice Suh <justice.suh@gmail.com>
2024-12-28 16:30:02 -0500

    Remove gnome-games and libreoffice

--- install.sh
+++ install.sh
@@ -4,10 +4,18 @@ sudo apt update
 sudo apt install -y curl git vim
 
 if ! command -v zsh > /dev/null 2>&1; then
-	sudo apt install -y zsh
-	sudo chsh -s $(which zsh)
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sudo apt install -y zsh
+    sudo chsh -s $(which zsh)
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 gsettings set org.gnome.desktop.interface text-scaling-factor 1.25
+
+if dpkg -s gnome-games | grep "Status" > /dev/null 2>&1; then
+    sudo apt purge --autoremove -y gnome-games
+fi
+if command -v libreoffice > /dev/null 2>&1; then
+    sudo apt purge --autoremove -y "libreoffice*"
+fi
+sudo apt clean
 
