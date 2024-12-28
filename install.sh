27c8aa01ef1f472c1581e8ce82fb64984cdb89cf
Justice Suh <justice.suh@gmail.com>
2024-12-28 16:40:40 -0500

    Install docker

--- install.sh
+++ install.sh
@@ -19,3 +19,12 @@ if command -v libreoffice > /dev/null 2>&1; then
 fi
 sudo apt clean
 
+if ! command -v docker > /dev/null 2>&1; then
+    sudo apt install -y apt-transport-https ca-certificates gnupg
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/debian bookworm stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt update
+    sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo usermod -aG docker ${USER}
+fi
+
