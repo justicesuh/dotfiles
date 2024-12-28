b48d8b2042c2be8ed7f0ca2d2f80cec5212fa3fe
Justice Suh <justice.suh@gmail.com>
2024-12-28 14:17:29 -0500

    Install zsh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install -y curl git vim
+
+if ! command -v zsh > /dev/null 2>&1; then
+	sudo apt install -y zsh
+	sudo chsh -s $(which zsh)
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
