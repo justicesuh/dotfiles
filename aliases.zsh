727e51dc98e91b2add207c7565ce1c2219a798bf
Justice Suh <justice.suh@gmail.com>
2024-12-29 15:53:11 -0500

    Add cls alias

--- aliases.zsh
+++ aliases.zsh
@@ -4,6 +4,7 @@ alias v="xclip -selection clipboard -o"
 alias pubkey="c < $HOME/.ssh/id_ed25519.pub"
 alias shrug="echo '¯\_(ツ)_/¯' | c"
 
+alias cls="clear"
 alias dotfiles="cd $DOTFILES"
 
 alias wifi="nmcli dev wifi show-password"
