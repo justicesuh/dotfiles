444d5ca31b2b536de1f81c38ff25279df9bc1c8b
Justice Suh <justice.suh@gmail.com>
2024-12-29 13:51:34 -0500

    Install taskwarrior

--- symlinks.sh
+++ symlinks.sh
@@ -4,6 +4,7 @@ DOTFILES=$(realpath $0 | xargs dirname)
 
 ln -sf $DOTFILES/vscode/settings.json $HOME/.config/Code/User/settings.json
 ln -sf $DOTFILES/.gitconfig $HOME/.gitconfig
+ln -sf $DOTFILES/.taskrc $HOME/.taskrc
 ln -sf $DOTFILES/.vimrc $HOME/.vimrc
 ln -sf $DOTFILES/.zshrc $HOME/.zshrc
 ln -sf $DOTFILES/aliases.zsh $HOME/.oh-my-zsh/custom/aliases.zsh
