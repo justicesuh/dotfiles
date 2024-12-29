6aa3ee97fd0c49916f563c744b4615b05f70a553
Justice Suh <justice.suh@gmail.com>
2024-12-28 21:23:23 -0500

    Add .gitconfig

--- symlinks.sh
+++ symlinks.sh
@@ -3,6 +3,7 @@
 DOTFILES=$(realpath $0 | xargs dirname)
 
 ln -sf $DOTFILES/vscode/settings.json $HOME/.config/Code/User/settings.json
+ln -sf $DOTFILES/.gitconfig $HOME/.gitconfig
 ln -sf $DOTFILES/.vimrc $HOME/.vimrc
 ln -sf $DOTFILES/.zshrc $HOME/.zshrc
 ln -sf $DOTFILES/aliases.zsh $HOME/.oh-my-zsh/custom/aliases.zsh
