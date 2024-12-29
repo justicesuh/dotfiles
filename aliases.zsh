68745febb678b5639198307cc75fa84f97d274fb
Justice Suh <justice.suh@gmail.com>
2024-12-29 12:29:28 -0500

    Alias cd dotfiles

--- aliases.zsh
+++ aliases.zsh
@@ -4,6 +4,8 @@ alias v="xclip -selection clipboard -o"
 alias pubkey="c < $HOME/.ssh/id_ed25519.pub"
 alias shrug="echo '¯\_(ツ)_/¯' | c"
 
+alias dotfiles="cd $DOTFILES"
+
 alias wifi="nmcli dev wifi show-password"
 
 alias python="python3"
