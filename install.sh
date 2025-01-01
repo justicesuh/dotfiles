4f458c465288d3c3f6733138d90078313b2b3b29
Justice Suh <justice.suh@gmail.com>
2025-01-01 12:09:26 -0500

    Install spotify

--- install.sh
+++ install.sh
@@ -49,6 +49,13 @@ if ! command -v code > /dev/null 2>&1; then
     sudo apt install -y code
 fi
 
+if ! is_pkg_installed spotify-client; then
+    curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+    echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+    sudo apt update
+    sudo apt install -y spotify-client
+fi
+
 install_pkg firefox-esr
 install_pkg python3
 install_pkg python3-pip
