6062a4ded9f1ce9380c7dec88bf43fdc107e1443
Justice Suh <justice.suh@gmail.com>
2026-02-26 11:58:43 -0500

    Add gpg keys from json

--- setup.sh
+++ setup.sh
@@ -15,8 +15,25 @@ install_package() {
     fi
 }
 
+install_packages() {
+    for i in "$@"; do
+        install_package $i
+    done
+}
+
+add_keys() {
+    install_packages apt-transport-https ca-certificates gnupg jq
+    for key in $(jq -c '.[]' "$DOTFILES/keys.json"); do
+        url=$(echo "$key" | jq -r '.url')
+        location=$(echo "$key" | jq -r '.location')
+        if [ ! -f "$location" ]; then
+            sudo mkdir -p "$(dirname "$location")"
+            curl -fsSL "$url" | sudo gpg --dearmor -o "$location"
+        fi
+    done
+}
+
 install_apt_packages() {
-    sudo apt update
     for package in $(cat "$DOTFILES/apt_packages"); do
         install_package $package
     done
@@ -43,8 +60,6 @@ setup_symlinks() {
 
 setup_docker() {
     if ! command -v docker > /dev/null 2>&1; then
-        sudo apt install -y apt-transport-https ca-certificates gnupg
-        curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg
         echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/debian trixie stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
         sudo apt update
         sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
@@ -54,15 +69,17 @@ setup_docker() {
 
 setup_vscode() {
     if ! command -v code > /dev/null 2>&1; then
-        curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /etc/apt/keyrings/microsoft-archive-keyring.gpg
         sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
         sudo apt update
         sudo apt install -y code
 fi
 }
-#setup_symlinks
+setup_symlinks
+add_keys
 #setup_docker
 #setup_vscode
 
-install_apt_packages
+#add_keys
+#sudo apt update
+#install_apt_packages
 
