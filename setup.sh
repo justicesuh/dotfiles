304578f0d6d58f154398a939cdcfa04736789ce5
Justice Suh <justice.suh@gmail.com>
2026-02-25 22:19:55 -0500

    Set up symlinks

--- setup.sh
+++ setup.sh
@@ -13,8 +13,19 @@ install_pkg() {
     fi
 }
 
-if install_pkg zsh; then
-    sudo chsh -s $(which zsh)
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi
+setup_zsh() {
+    if install_pkg zsh; then
+        sudo chsh -s $(which zsh)
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    fi
+}
 
+setup_symlinks() {
+	find $(realpath $0 | xargs dirname) -maxdepth 1 -mindepth 1 -name '.*' ! -name '.git' |
+    while IFS= read -r f; do
+        ln -sf $f $HOME/${f##*/}
+    done
+}
+
+setup_zsh
+setup_symlinks
